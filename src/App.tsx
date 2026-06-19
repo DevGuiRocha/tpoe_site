d6992f4391c46eda4a00fa24a746c17a8cc3c550
Guilherme Rocha <jguilherme.1997@gmail.com>
2026-06-19 13:42:30 -0300

    Feat: adição de rotas para o tipo de famílias e também cada família individualmente

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
-import BienvenuerPage from './pages/BienvenuePage'
+import BienvenuePage from './pages/BienvenuePage'
+import FamiliesPage from './pages/FamiliesPage'
+import FamilyDetailPage from './pages/FamilyDetailPage'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/enredo" element={<BienvenuerPage />} />
+        <Route path="/enredo" element={<BienvenuePage />} />
+        <Route path="/familias/alfa" element={<FamiliesPage tier="Alfa" />} />
+        <Route path="/familias/beta" element={<FamiliesPage tier="Beta" />} />
+        <Route path="/familias/gama" element={<FamiliesPage tier="Gama" />} />
+        <Route path="/familias/:slug" element={<FamilyDetailPage />} />
       </Routes>
     </BrowserRouter>
   )
