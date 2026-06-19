d0659b3c1a4f10c701822ae9dc41996ecc0224b7
Guilherme Rocha <jguilherme.1997@gmail.com>
2026-06-19 16:47:24 -0300

    Refactor: Correção do nome de uma das ADMs

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
         <p className={styles.brand}>Mônaco</p>
         <p className={styles.sub}>The Paradise On Earth &mdash; 2032</p>
         <p className={styles.credits}>
-          Um projeto de roleplay criado por Júlia, Andressa, Ana's, Emilly, Kamilly, Kemili &amp; Rafaela
+          Um projeto de roleplay criado por Júlia, Andressa, Ana's, Emilly, Kamilly, Kemili &amp; Rafaella
         </p>
       </div>
     </footer>
